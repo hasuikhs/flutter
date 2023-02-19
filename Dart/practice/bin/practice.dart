820941fd0eef2d55edb3e06452a373c2157868d3
hanseok kim <hasuikhs@gmail.com>
2023-02-19 22:16:28 +0900

    230219|Update dart type

--- Dart/practice/bin/practice.dart
+++ Dart/practice/bin/practice.dart
@@ -1,5 +1,28 @@
 import 'package:practice/practice.dart' as practice;
 
+/**
+ * Numbers(int(정수), double(실수), num(정수 실수 포함))
+ * Strings(String(문자열))
+ * Booleans(bool(true, false))
+ * Lists(List)
+ * Sets(Set)
+ * Maps(Map)
+ */
+
 void main(List<String> arguments) {
   print('Hello world: ${practice.calculate()}!');
+
+  print('${ practice.add(1, 2)}');
+
+  var variable1 = 'Bob';
+  variable1 = 'hoho';
+  print(variable1);
+
+  dynamic variable2 = 'dynamic';
+  print(variable2);
+
+  variable2 = 1234;
+  print(variable2);
+
+  print(false == true);
 }
--- Dart/practice/lib/practice.dart
+++ Dart/practice/lib/practice.dart
@@ -1,3 +1,7 @@
 int calculate() {
   return 6 * 7;
 }
+
+int add(int a, int b) {
+  return a + b;
+}
